14bdc0d0d30f22b96a7e92df0fd2272b6d5d15af
Shane Smiskol <shane@smiskol.com>
2020-12-05 04:04:56 -0600

    clean up a bit

--- steer_delay/steer_delay.py
+++ steer_delay/steer_delay.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import warnings
+
 warnings.simplefilter(action='ignore', category=FutureWarning)  # for seaborn
 import os
 import pickle
@@ -25,21 +26,21 @@ def to_signed(n, bits):
 
 
 def find_steer_delay(plot=False):
-  use_dir = '/openpilot/steer_delay/rlogs/good'
+  use_dir = '/openpilot/steer_delay/rlogs/good'  # change to a directory of rlogs
+  files = os.listdir(use_dir)
+  files = [f for f in files if '.ini' not in f]
 
-  routes = [[os.listdir(use_dir)[0]]]
-  for rt in os.listdir(use_dir)[1:]:
+  routes = [[files[0]]]  # this mess ensures we process each route's segments independantly since sorting will join samples from random routes
+  for rt in files[1:]:  # todo: clean up
     rt_name = ''.join(rt.split('--')[:2])
     if rt_name != ''.join(routes[-1][-1].split('--')[:2]):
       routes.append([rt])
     else:
       routes[-1].append(rt)
 
-  print(routes)
   lrs = []
   for _routes in routes:
     lrs.append(MultiLogIterator([os.path.join(use_dir, i) for i in _routes], wraparound=False))
-
 
   data = [[]]
   for lr in lrs:
@@ -69,9 +70,8 @@ def find_steer_delay(plot=False):
         elif m.address == 0x25 and m.src == 0:  # STEER_ANGLE_SENSOR
           steer_angle = to_signed(int(bin(m.dat[0])[2:].zfill(8)[4:] + bin(m.dat[1])[2:].zfill(8), 2), 12) * 1.5
 
-      if engaged and not steering_pressed and None not in [torque_cmd, steer_angle, yaw_rate, v_ego]:  # creates uninterupted sections of engaged data
-        if v_ego < 10 * CV.MPH_TO_MS:  # todo: experiment with 5 or lower
-          continue
+      if (engaged and not steering_pressed and None not in [torque_cmd, steer_angle, yaw_rate, v_ego] and  # creates uninterupted sections of engaged data
+              v_ego > 1 * CV.MPH_TO_MS):  # todo: experiment with 5 or lower
         data[-1].append({'engaged': engaged, 'torque_cmd': torque_cmd, 'steering_pressed': steering_pressed,
                          'steer_angle': steer_angle, 'time': msg.logMonoTime * 1e-9, 'yaw_rate': yaw_rate, 'v_ego': v_ego})
       elif len(data[-1]):
@@ -83,16 +83,14 @@ def find_steer_delay(plot=False):
   del data
   assert len(split) > 0, "Not enough valid sections of samples"
 
-  # print('max seq len: {}'.format(max([len(line) for line in split])))
-  # print([len(line) for line in split])
-
   delays = []
   ptps = []
   seq_lens = []
+  new_split = []
+  mean_vels = []
   for idx, data in enumerate(split):
     torque = np.array([line['torque_cmd'] for line in data])
     angles = np.array([line['steer_angle'] for line in data])
-    # rates = np.array([line['yaw_rate'] for line in data])
     if angles.std() == 0 or torque.std() == 0:
       print('warning: angles or torque std is 0! skipping...')
       continue
@@ -100,31 +98,31 @@ def find_steer_delay(plot=False):
     if angles_ptp <= 10:
       print('angle range too low ({} <= 10)! skipping...'.format(angles_ptp))
       continue
-    print('peak to peak: {}'.format(angles.ptp(), np.ptp(angles)))
+    print('peak to peak: {}'.format(angles_ptp))
 
-    # diff = max(torque) / max(angles)  # todo: not sure which normalization method is better. scale
+    # diff = max(torque) / max(angles)  # todo: not sure which regularization method is better. normalization
     # angles *= diff
 
-    Y1 = (angles - angles.mean()) / angles.std()  # todo: or regularization
+    Y1 = (angles - angles.mean()) / angles.std()  # todo: or standardization
     Y2 = (torque - torque.mean()) / torque.std()
-    # rates = (rates - rates.mean()) / rates.std()
 
     plt.clf()
-    # plt.ylim(-7, 10)
     plt.title('before offset')
     plt.plot(angles, label='angle')
     plt.plot(torque / (max(torque) / max(angles)), label='torque')
-    # plt.plot(rates, label='rate')
     plt.legend()
     plt.savefig('/openpilot/steer_delay/plots/{}__before.png'.format(idx))
 
-    xcorr = correlate(Y1, Y2)[angles.size - 1:]  # indexing forces positive offset (torque always precedes angle)
-    time_shift = xcorr.argmax()
+    # xcorr = correlate(Y1, Y2)[angles.size - 1:]  # indexing forces positive offset (torque always precedes angle)
+    xcorr = correlate(Y1, Y2)  # indexing forces positive offset (torque always precedes angle)
+    time_shift = np.arange(1 - angles.size, angles.size)[xcorr.argmax()]
 
-    if time_shift > 0:
+    if 0 < time_shift < 100:  # still plot them to debug todo: remove them
       delays.append(time_shift)
       ptps.append(angles_ptp)
       seq_lens.append(len(data))
+      new_split.append(split)
+      mean_vels.append(np.mean([line['v_ego'] for line in data]))
 
     print('len: {}'.format(len(data)))
     print('time shift: {}'.format(time_shift))
@@ -132,11 +130,9 @@ def find_steer_delay(plot=False):
 
     torque = np.roll(torque, time_shift)
     plt.clf()
-    # plt.ylim(-7, 10)
     plt.title('after offset ({})'.format(time_shift))
     plt.plot(angles, label='angle')
-    plt.plot(torque/(max(torque) / max(angles)), label='torque')
-    # plt.plot(rates, label='rate')
+    plt.plot(torque / (max(torque) / max(angles)), label='torque')
     plt.legend()
     plt.savefig('/openpilot/steer_delay/plots/{}_after.png'.format(idx))
 
@@ -146,9 +142,9 @@ def find_steer_delay(plot=False):
   plt.savefig('/openpilot/steer_delay/dist.png')
 
   with open('/openpilot/steer_delay/data', 'wb') as f:
-    pickle.dump(split, f)
+    pickle.dump(new_split, f)
 
-  print('mean vels:  {}'.format([np.round(np.mean([line['v_ego'] * CV.MS_TO_MPH for line in sec]), 2) for sec in split]))  # todo: add vel list
+  print('mean vels:  {}'.format(np.round(np.array(mean_vels) * CV.MS_TO_MPH, 2).tolist()))  # todo: add vel list
   print('seq lens:   {}'.format(seq_lens))
   print('ptp angles: {}'.format(ptps))
   print('delays:     {}'.format(delays))
