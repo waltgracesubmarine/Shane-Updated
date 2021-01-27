742d7b6dc46a210d8e9d8593045b6d4252463ecb
Shane Smiskol <shane@smiskol.com>
2021-01-27 14:30:20 -0600

    use the function to convert the entire control output when > than some threshold

--- common/op_params.py
+++ common/op_params.py
@@ -100,10 +100,11 @@ class opParams:
                         'corollaTSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 Corolla'),
                         'rav4TSS2_use_indi': Param(False, bool, 'Enable this to use INDI for lat with your TSS2 RAV4'),
                         # 'gas_output': Param(0.0, VT.number, live=True),
+                        'min_accel': Param(-0.1, VT.number, 'The minumum control acceleration before comp_gb to use the accel to gas function. Default is -0.5'),
                         'long_kp_multiplier': Param(1, VT.number, live=True),
                         'long_ki_multiplier': Param(1, VT.number, live=True),
-                        'gb_reduction': Param(3., VT.number, 'The reduction to apply to just PI when under 19 mph and we\'re not braking', live=True),
-                        'new_compute_gb_method': Param(True, bool, 'Try enabling and disabling to see what works best', live=True),
+                        # 'gb_reduction': Param(3., VT.number, 'The reduction to apply to just PI when under 19 mph and we\'re not braking', live=True),
+                        # 'new_compute_gb_method': Param(True, bool, 'Try enabling and disabling to see what works best', live=True),
                         'standstill_hack': Param(False, bool, 'Some cars support stop and go, you just need to enable this')}
 
     self._params_file = '/data/op_params.json'
