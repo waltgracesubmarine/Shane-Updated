badfd7b32c3c38123172a136ec1255703af33f98
Shane Smiskol <shane@smiskol.com>
2021-02-01 23:47:45 -0600

    Try without coast function

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -44,7 +44,7 @@ def compute_gb_gas_interceptor(accel, speed):
 
   if speed <= MIN_ACC_SPEED:
     # todo: interpolate output near coast_accel, make it a smooth transition (but with the coast function it should be decent right now)
-    return accel_to_gas(accel, speed) if accel > coast_accel(speed) else compute_gb_toyota(accel, speed)
+    return accel_to_gas(accel, speed) if accel > -0.2 else compute_gb_toyota(accel, speed)
   return compute_gb_toyota(accel, speed)
 
 
