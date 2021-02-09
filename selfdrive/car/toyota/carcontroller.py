9d83892c049501e9ee26b92f9ea39bd1fbc247d7
Shane Smiskol <shane@smiskol.com>
2021-02-08 22:12:00 -0600

    Test

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -94,7 +94,7 @@ class CarController():
       # +0.06 offset to reduce ABS pump usage when OP is engaged
       if apply_accel * ACCEL_SCALE > min_pedal_accel:  # if requesting more than coasting accel, apply gas. still send positive accel when available no matter what
         apply_gas = compute_gb_gas_interceptor(apply_accel * ACCEL_SCALE, CS.out.vEgo)
-        # apply_accel = 0.06 - actuators.brake
+        apply_accel = 0.06 - actuators.brake
       # elif apply_accel <= 0:
       #   apply_accel = 0.06 - actuators.brake
       # else:  # if not higher than coast accel, but not less than 0, send positive accel so car can decide how much to brake/coast
