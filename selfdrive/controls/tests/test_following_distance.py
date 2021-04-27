8a428e8f4f6b4acd42aa17f788388fa20aec0b64
Shane Smiskol <shane@smiskol.com>
2021-04-27 00:16:03 -0500

    debug

--- selfdrive/controls/tests/test_following_distance.py
+++ selfdrive/controls/tests/test_following_distance.py
@@ -88,5 +88,10 @@ class TestFollowingDistance(unittest.TestCase):
 
       simulation_steady_state = run_following_distance_simulation(v_lead)
       correct_steady_state = RW(v_lead, v_lead) + 4.0
+      print(f'v_lead: {v_lead}, sim: {simulation_steady_state}, correct: {correct_steady_state}')
 
       self.assertAlmostEqual(simulation_steady_state, correct_steady_state, delta=0.1)
+
+
+tfd = TestFollowingDistance()
+tfd.test_following_distanc()
