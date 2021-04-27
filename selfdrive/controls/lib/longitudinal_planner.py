eb99c0015ec56425327156067b260ae5e0142683
Shane Smiskol <shane@smiskol.com>
2021-04-26 19:49:39 -0500

    fix another mistake

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -103,7 +103,7 @@ class Planner():
         self.v_acc = self.mpc2.v_mpc
         self.a_acc = self.mpc2.a_mpc
         self.a_acc_start = self.mpc2.a_mpc_start
-        self.v_acc_start = self.mpc2.a_mpc_start
+        self.v_acc_start = self.mpc2.v_mpc_start
       elif slowest == 'cruise':
         self.v_acc = self.v_cruise
         self.a_acc = self.a_cruise
@@ -129,9 +129,6 @@ class Planner():
 
     enabled = (long_control_state == LongCtrlState.pid) or (long_control_state == LongCtrlState.stopping)
     following = lead_1.status and lead_1.dRel < 45.0 and lead_1.vLeadK > v_ego and lead_1.aLeadK > 0.0
-
-    # self.v_acc_start = self.v_acc_next
-    # self.a_acc_start = self.a_acc_next
 
     # Calculate speed for normal cruise control
     if enabled and not self.first_loop and not sm['carState'].gasPressed:
@@ -185,12 +182,6 @@ class Planner():
                                        lead_1.fcw, blinkers) and not sm['carState'].brakePressed
     if self.fcw:
       cloudlog.info("FCW triggered %s", self.fcw_checker.counters)
-
-    # Interpolate 0.05 seconds and save as starting point for next iteration
-    # a_acc_sol = self.a_acc_start + (CP.radarTimeStep / LON_MPC_STEP) * (self.a_acc - self.a_acc_start)
-    # v_acc_sol = self.v_acc_start + CP.radarTimeStep * (a_acc_sol + self.a_acc_start) / 2.0
-    # self.v_acc_next = v_acc_sol
-    # self.a_acc_next = a_acc_sol
 
     self.first_loop = False
 
