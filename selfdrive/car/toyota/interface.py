dda28b51c842ffc4a1f4ff0232c27f7e4d03c37c
Shane Smiskol <shane@smiskol.com>
2021-05-02 16:00:23 -0500

    add back for ref

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -283,6 +283,8 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kf = 0.00007818594
         # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[.028], [.0012]]  # birdman6450#7399's Corolla 2020 PIF Tune
         # ret.lateralTuning.pid.kdV = [0.]
+        # ret.lateralTuning.pid.kf = 0.000153263811757641
+        # ret.lateralTuning.pid.newKfTuned = True
         # ret.steerActuatorDelay = 0.48 - 0.2
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
