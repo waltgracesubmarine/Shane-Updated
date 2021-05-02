970e24241278ecae4543ca8890712d100c777caa
Shane Smiskol <shane@smiskol.com>
2021-05-02 16:04:30 -0500

    add back for ref

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -50,6 +50,8 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
+      # ret.lateralTuning.pid.kf = 0.00007843996051470903
+      # ret.lateralTuning.pid.newKfTuned = True
 
     if candidate == CAR.FORESTER:
       ret.mass = 1568. + STD_CARGO_KG
