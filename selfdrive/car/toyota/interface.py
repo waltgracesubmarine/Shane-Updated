04d0955eb876251d779e4df0713c5e33767f243d
Shane Smiskol <shane@smiskol.com>
2021-05-09 14:10:47 -0700

    adjust delay for trae

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -68,7 +68,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 13.4  # unknown end-to-end spec
       tire_stiffness_factor = 0.6371  # hand-tune
       ret.mass = 3115. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.steerActuatorDelay = 0.3
+      ret.steerActuatorDelay = 0.33
 
       if prius_use_pid:
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.05]]
