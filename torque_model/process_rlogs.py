d9ccd1eb0960182c4b8574576818c1bb75c3ad35
Shane Smiskol <shane@smiskol.com>
2021-08-25 00:05:25 -0500

    process_rlogs update

--- torque_model/process_rlogs.py
+++ torque_model/process_rlogs.py
@@ -2,13 +2,15 @@
 from opendbc.can.parser import CANParser
 from tools.lib.logreader import MultiLogIterator
 from cereal import car
+from selfdrive.car.toyota.values import CAR as TOYOTA_CAR, DBC as TOYOTA_DBC
 from tqdm import tqdm   # type: ignore
 import pickle
 import os
+os.chdir('/home/shane/Git/op-torque')
 
 DT_CTRL = 0.01
 MIN_SAMPLES = 5 / DT_CTRL  # seconds to frames
-os.chdir('/home/shane/Git/op-torque')
+TRAIN_CARS = [TOYOTA_CAR.COROLLA, TOYOTA_CAR.PRIUS_TSS2]
 
 
 def load_and_process_rlogs(lrs, file_name):
@@ -16,7 +18,7 @@ def load_and_process_rlogs(lrs, file_name):
 
   for lr in lrs:
     engaged, v_ego, a_ego, gear_shifter = None, None, None, None
-    steering_angle, des_steering_angle, des_steering_rate = None, None, None
+    steering_angle, des_steering_angle, steering_rate, des_steering_rate = None, None, None, None
     last_engaged = None
 
     last_time = 0
@@ -28,25 +30,33 @@ def load_and_process_rlogs(lrs, file_name):
       ("STEER_TORQUE_CMD", "STEERING_LKA", 0),
       ("STEER_TORQUE_DRIVER", "STEER_TORQUE_SENSOR", 0),
       ("STEER_TORQUE_EPS", "STEER_TORQUE_SENSOR", 0),
-      ("STEER_RATE_FRACTION", "STEER_ANGLE_SENSOR", 0),
       ("STEER_RATE", "STEER_ANGLE_SENSOR", 0),
     ]
-    cp = CANParser("toyota_corolla_2017_pt_generated", signals)
+    cp = None
+    car_fingerprint = None
 
     all_msgs = sorted(lr, key=lambda msg: msg.logMonoTime)
-
     for msg in tqdm(all_msgs):
       if msg.which() == 'carState':
         v_ego = msg.carState.vEgo
         a_ego = msg.carState.aEgo
         steering_angle = msg.carState.steeringAngleDeg
+        steering_rate = msg.carState.steeringRateDeg
         last_engaged = bool(engaged)
         engaged = msg.carState.cruiseState.enabled
         gear_shifter = msg.carState.gearShifter
       # elif msg.which() == 'carControl':  # todo: maybe get eps torque
       #   apply_accel = msg.carControl.actuators.gas - msg.carControl.actuators.brake
+      elif msg.which() == 'carParams':
+        car_fingerprint = msg.carParams.carFingerprint
+        if car_fingerprint not in TRAIN_CARS:
+          raise Exception("Car not supported for training at this time: {}".format(car_fingerprint))
+        cp = CANParser(TOYOTA_DBC[car_fingerprint], signals)
 
-      if msg.which() not in ['can', 'sendcan']:
+      if cp is None:  # no carParams msg yet
+        continue
+
+      if msg.which() != 'can':  # only store when can is updated
         continue
       cp_updated = cp.update_string(msg.as_builder().to_bytes())  # usually all can signals are updated so we don't need to iterate through the updated list
 
@@ -54,6 +64,10 @@ def load_and_process_rlogs(lrs, file_name):
         if u == 0x3bc:  # GEAR_PACKET
           can_updated = True
 
+      if not can_updated or engaged is None:
+        continue
+
+      # FIXME: this is probably all Toyota (Corolla) specific
       sport_on = bool(cp.vl['GEAR_PACKET']['SPORT_ON'])
       user_override = abs(cp.vl['STEER_TORQUE_SENSOR']['STEER_TORQUE_DRIVER']) > 100
 
@@ -62,27 +76,19 @@ def load_and_process_rlogs(lrs, file_name):
       torque_driver = cp.vl['STEER_TORQUE_SENSOR']['STEER_TORQUE_DRIVER']
       steer_req = cp.vl['STEERING_LKA']['STEER_REQUEST'] == 1
 
-      steering_rate = cp.vl['STEER_ANGLE_SENSOR']['STEER_RATE'] + cp.vl['STEER_ANGLE_SENSOR']['STEER_RATE_FRACTION']
-      # steering_rate_fraction = cp.vl['STEER_ANGLE_SENSOR']['STEER_RATE_FRACTION']
-
-      if msg.which() != 'can':  # only store when can is updated
-        continue
-
       if abs(msg.logMonoTime - last_time) * 1e-9 > 1 / 20:
         print('TIME BREAK!')
         print(abs(msg.logMonoTime - last_time) * 1e-9)
 
-      should_gather = not engaged
-      if engaged and steer_req and not user_override:
-        should_gather = True
+      should_gather = not engaged or (engaged and steer_req and not user_override)
+      should_gather = should_gather
 
-      if (engaged is not None and can_updated and gear_shifter == car.CarState.GearShifter.drive and
-              not sport_on and should_gather and engaged == last_engaged and  # creates uninterupted sections of engaged data
+      # creates uninterupted sections of engaged data
+      if (should_gather and gear_shifter == car.CarState.GearShifter.drive and not sport_on and engaged == last_engaged and
               abs(msg.logMonoTime - last_time) * 1e-9 < 1 / 20):  # also split if there's a break in time
         data[-1].append({'v_ego': v_ego, 'a_ego': a_ego, 'steering_angle': steering_angle, 'steering_rate': steering_rate,
-                         # 'steering_rate_fraction': steering_rate_fraction,
                          'engaged': engaged, 'torque_cmd': torque_cmd, 'torque_eps': torque_eps, 'torque_driver': torque_driver,
-                         'time': msg.logMonoTime * 1e-9})
+                         'car_fingerprint': car_fingerprint, 'time': msg.logMonoTime * 1e-9})
       elif len(data[-1]):  # if last list has items in it, append new empty section
         data.append([])
 
