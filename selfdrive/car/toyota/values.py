0c32b716afcc2bc79b6c94339b003b127be70f00
Shane Smiskol <shane@smiskol.com>
2021-11-02 01:52:03 -0500

    Revert "update tune, rate limits not permanent"
    This reverts commit 806ffdf5ddf43e47a43e8f7228bde1210ee9e596.

--- selfdrive/car/toyota/values.py
+++ selfdrive/car/toyota/values.py
@@ -16,9 +16,9 @@ class CarControllerParams:
   ACCEL_MIN = -3.5  # m/s2
 
   STEER_MAX = 1500
-  STEER_DELTA_UP = 12       # 1.5s time to peak torque
+  STEER_DELTA_UP = 10       # 1.5s time to peak torque
   STEER_DELTA_DOWN = 25     # always lower than 45 otherwise the Rav4 faults (Prius seems ok with 50)
-  STEER_ERROR_MAX = 450     # max delta between torque cmd and torque motor
+  STEER_ERROR_MAX = 350     # max delta between torque cmd and torque motor
 
 class CAR:
   # Toyota
