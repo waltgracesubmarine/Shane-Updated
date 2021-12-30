589d9e2402d56d73427c51f2acc223e372bd3767
Shane Smiskol <shane@smiskol.com>
2021-12-29 17:19:59 -0700

    Longcontrol: remove starting state (#543)
    * remove starting state
    
    * remove params

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -316,10 +316,8 @@ class CarInterface(CarInterfaceBase):
       set_long_tune(ret.longitudinalTuning, LongTunes.TSS2)
       ret.vEgoStopping = 0.2  # car is near 0.1 to 0.2 when car starts requesting stopping accel
       ret.vEgoStarting = 0.2  # needs to be > or == vEgoStopping
-      ret.startAccel = 0.0  # Toyota requests 0 instantly, then hands control off to some controller
       ret.stopAccel = -2.0  # Toyota requests -0.4 when stopped
       ret.stoppingDecelRate = 0.8  # reach stopping target smoothly - seems to take 0.5 seconds to go from 0 to -0.4
-      ret.startingAccelRate = 20.  # release brakes fast
     else:
       set_long_tune(ret.longitudinalTuning, LongTunes.TSS)
 
