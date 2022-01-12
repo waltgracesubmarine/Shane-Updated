82125052cbef7454d8980241a3b04ec0ecf0e934
Shane Smiskol <shane@smiskol.com>
2022-01-12 07:03:59 -0700

    accelnet

--- accelnet/accelnet.py
+++ accelnet/accelnet.py
@@ -3,11 +3,17 @@
 # warnings.simplefilter(action='ignore', category=FutureWarning)
 import os
 import sys
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LeakyReLU, GaussianNoise, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adagrad, Adadelta, Adam
+
+from sklearn.model_selection import train_test_split
 
 from opendbc.can.parser import CANParser
 from tools.lib.logreader import MultiLogIterator
 from tools.lib.route import Route
 from cereal import car
+from common.filter_simple import FirstOrderFilter
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -37,6 +43,13 @@ def tokenize(data, seq_length):
   return seq
 
 
+def split_list(l, n, enforce_len=True):
+  output = []
+  for i in range(0, len(l), n):
+    output.append(l[i:i + n])
+  return [i for i in output if (len(i) == n and enforce_len) or not enforce_len]
+
+
 def feedforward(v_ego, angle_steers, kf=1.):  # outputs unscaled ~lateral accel, with optional kf param to scale
   steer_feedforward = angle_steers * v_ego ** 2
   return steer_feedforward * kf
@@ -60,14 +73,22 @@ def load_and_process_rlogs(lr, file_name):
     'carState': {
       'vEgo': None,
       'aEgo': None,
-      'gasPressed': None
+      'gasPressed': None,
     },
     'controlsState': {
-      'enabled': False
+      'enabled': False,
+    },
+    'longitudinalPlan': {
+      'jerks': None,
+      'accels': None
     }
   }
+  jEgo = 0.
+  jEgo_period = 50  # frames/100
   last_time = 0
   can_updated = False
+  accel_filter = FirstOrderFilter(0, jEgo_period * DT_CTRL, DT_CTRL)
+  filter_updates = 0.
 
   can_signals = [
     ("ACCEL_CMD", "ACC_CONTROL", 0),
@@ -85,6 +106,12 @@ def load_and_process_rlogs(lr, file_name):
     if msg.which() != 'sendcan':
       continue
 
+    if log_msgs['carState']['aEgo'] is not None:
+      accel_filter.update(log_msgs['carState']['aEgo'])
+      filter_updates += 1
+      if filter_updates > jEgo_period:
+        jEgo = (log_msgs['carState']['aEgo'] - accel_filter.x) * ((1 / DT_CTRL) / jEgo_period)
+
     cp_updated = cp.update_string(msg.as_builder().to_bytes())
     for u in cp_updated:
       if u == 0x343:  # ACC_CONTROL
@@ -92,17 +119,23 @@ def load_and_process_rlogs(lr, file_name):
 
     accel_cmd = float(cp.vl['ACC_CONTROL']['ACCEL_CMD'])
 
-    sample_ok = None not in [log_msgs['carState']['vEgo'], log_msgs['carState']['aEgo'], accel_cmd] and can_updated
+    sample_ok = log_msgs['carState']['vEgo'] is not None and can_updated
     sample_ok = sample_ok and log_msgs['controlsState']['enabled'] and not log_msgs['carState']['gasPressed']
+    sample_ok = sample_ok and filter_updates > jEgo_period
 
     # creates uninterupted sections of engaged data
     if sample_ok and abs(msg.logMonoTime - last_time) * 1e-9 < 1 / 20:  # also split if there's a break in time
-      to_append = {'accel_cmd': accel_cmd, 'time': msg.logMonoTime * 1e-9}
+      to_append = {'accel_cmd': accel_cmd, 'jEgo': jEgo, 'time': msg.logMonoTime * 1e-9}
       for log_signals in log_msgs.values():
         to_append = {**to_append, **log_signals}
+      for i in to_append:
+        if 'capnp' in str(type(to_append[i])):
+          to_append[i] = list(to_append[i])
       data[-1].append(to_append)
     elif len(data[-1]):  # if last list has items in it, append new empty section
       data.append([])
+      accel_filter.x = 0.
+      filter_updates = 0
     last_time = msg.logMonoTime
 
   del all_msgs
@@ -125,124 +158,183 @@ def main(lr):
 
   data_tokenized = []
   for seq in data:
-    data_tokenized += tokenize(seq, round(2. / DT_CTRL))
+    data_tokenized += tokenize(seq, round(0.31 / DT_CTRL))
   del data
 
-  print(f'One second sequences: {len(data_tokenized)}')
-  r = np.random.randint(25000)
-  plt.plot([i['aEgo'] for i in data_tokenized[r]], label='aEgo')
-  plt.plot([i['accel_cmd'] for i in data_tokenized[r]], label='acc_command')
-  plt.legend()
-  plt.pause(5)
-  input()
+  data_sequences = []
+  idx = 0
+  for seq in data_tokenized:
+    if idx > 1:
+      data_sequences.append(seq)
+      idx = 0
+    idx += 1
 
-  # Data preprocessing
-  for line in data:
-    line['angle_steers'] = abs(line['angle_steers'] - line['angle_offset'])  # need to offset angle to properly fit ff
-    line['torque'] = abs(line['torque'])
+  print(f'Tokenized sequences: {len(data_sequences)}')
+  if PLOT := False:
+    r = np.random.randint(25000)
+    plt.plot([i['aEgo'] for i in data_sequences[r]], label='aEgo')
+    # plt.plot([i['jEgo'] for i in data_sequences[r]], label='jEgo')
+    plt.plot([i['accel_cmd'] for i in data_sequences[r]], label='acc_command')
+    # plt.plot([i['jerks'][2] for i in data_sequences[r]], label='jerks')
+    plt.plot([i['accels'][0] for i in data_sequences[r]], label='accels')
+    plt.legend()
+    plt.show()
+    input()
 
-    del line['time'], line['angle_offset']  # delete unused
+  x_train = []
+  y_train = []
+  for seq in data_sequences:
+    # given current and future accel, output accel cmd we used to get there
+    # x_train.append([seq[0]['vEgo'], seq[0]['aEgo'], seq[15]['aEgo']])
+    x_train.append([seq[0]['aEgo'], seq[15]['aEgo']])
+    y_train.append(seq[0]['accel_cmd'])
 
-  data_speeds = np.array([line['v_ego'] for line in data])
-  data_angles = np.array([line['angle_steers'] for line in data])
-  data_torque = np.array([line['torque'] for line in data])
+  x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.4, random_state=42)
 
-  params, covs = curve_fit(_fit_kf, np.array([data_speeds, data_angles]), np.array(data_torque) / MAX_TORQUE,
-                           # maxfev=800
-                           )
-  fit_kf = params[0]
-  print('FOUND KF: {}'.format(fit_kf))
+  x_train = np.array(x_train)
+  y_train = np.array(y_train)
+  print('x_train shape: {}, y_train shape: {}'.format(x_train.shape, y_train.shape))
 
-  print()
+  model = Sequential()
+  # model.add(GaussianNoise(0.1, input_shape=(3,)))
+  model.add(Dense(8, input_shape=(2,), activation=LeakyReLU()))
+  # model.add(BatchNormalization())
+  # model.add(Dropout(0.2))
+  # model.add(Dense(4, activation='relu'))
+  # model.add(Dropout(1/8*.1))
+  model.add(Dense(4, activation=LeakyReLU()))
+  # model.add(Dropout(0.1))
+  model.add(Dense(1, activation='linear'))
 
-  std_func = []
-  fitted_func = []
-  for line in data:
-    std_func.append(abs(feedforward(line['v_ego'], line['angle_steers'], old_kf) * MAX_TORQUE - line['torque']))
-    fitted_func.append(abs(feedforward(line['v_ego'], line['angle_steers'], fit_kf) * MAX_TORQUE - line['torque']))
-  print('Function comparison on input data')
-  print('Torque MAE, current vs. fitted: {}, {}'.format(round(np.mean(std_func), 3), round(np.mean(fitted_func), 3)))
-  print('Torque STD, current vs. fitted: {}, {}'.format(round(np.std(std_func), 3), round(np.std(fitted_func), 3)))
+  opt = Adam(lr=0.001/2, amsgrad=True)
 
-  if SPEED_DATA_ANALYSIS := True:  # analyzes how torque needed changes based on speed
-    if PLOT_ANGLE_DIST := False:
-      sns.distplot([line['angle_steers'] for line in data if abs(line['angle_steers']) < 30], bins=200)
-      raise Exception
+  model.compile(opt, loss='mse', metrics='mae')
+  try:
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=90, batch_size=16)
+  except KeyboardInterrupt:
+    pass
 
-    res = 100
-    color = 'blue'
+  def show_preds(num=20):
+    rand_idxs = np.random.randint(len(x_test), size=num)
+    _x = np.take(x_test, rand_idxs, axis=0)
+    _y = np.take(y_test, rand_idxs, axis=0)
+    _y_pred = model.predict(_x)
 
-    _angles = [
-      [5, 10],
-      [10, 20],
-      [10, 15],
-      [15, 20],
-      [20, 25],
-      [20, 30],
-      [30, 45],
-    ]
+    plt.clf()
+    plt.plot(_y, label='ground truth')
+    plt.plot(_y_pred, label='prediction')
+    plt.legend()
+    plt.show()
+    plt.pause(0.01)
 
-    for idx, angle_range in enumerate(_angles):
-      angle_range_str = '{} deg'.format('-'.join(map(str, angle_range)))
-      temp_data = [line for line in data if angle_range[0] <= abs(line['angle_steers']) <= angle_range[1]]
-      if not len(temp_data):
-        continue
-      print(f'{angle_range} samples: {len(temp_data)}')
-      plt.figure()
-      speeds, torque = zip(*[[line['v_ego'], line['torque']] for line in temp_data])
-      plt.scatter(np.array(speeds) * CV.MS_TO_MPH, torque, label=angle_range_str, color=color, s=0.05)
+  while 1:
+    show_preds()
+    e = input()
+    if e == 'exit':
+      break
 
-      _x_ff = np.linspace(0, max(speeds), res)
-      _y_ff = [feedforward(_i, np.mean(angle_range), old_kf) * MAX_TORQUE for _i in _x_ff]
-      plt.plot(_x_ff * CV.MS_TO_MPH, _y_ff, color='orange', label='standard ff model at {} deg'.format(np.mean(angle_range)))
+  return model
 
-      _y_ff = [feedforward(_i, np.mean(angle_range), fit_kf) * MAX_TORQUE for _i in _x_ff]
-      plt.plot(_x_ff * CV.MS_TO_MPH, _y_ff, color='purple', label='new fitted ff function')
-
-      plt.legend()
-      plt.xlabel('speed (mph)')
-      plt.ylabel('torque')
-      plt.savefig('plots/{}.png'.format(angle_range_str))
-
-  if ANGLE_DATA_ANALYSIS := True:  # analyzes how angle changes need of torque (RESULT: seems to be relatively linear, can be tuned by k_f)
-    if PLOT_ANGLE_DIST := False:
-      sns.distplot([line['angle_steers'] for line in data if abs(line['angle_steers']) < 30], bins=200)
-      raise Exception
-
-    res = 100
-
-    _speeds = np.r_[[
-      [0, 10],
-      [10, 20],
-      [20, 30],
-      [30, 40],
-      [40, 50],
-      [50, 60],
-      [60, 70],
-    ]] * CV.MPH_TO_MS
-    color = 'blue'
-
-    for idx, speed_range in enumerate(_speeds):
-      speed_range_str = '{} mph'.format('-'.join([str(round(i * CV.MS_TO_MPH, 1)) for i in speed_range]))
-      temp_data = [line for line in data if speed_range[0] <= line['v_ego'] <= speed_range[1]]
-      if not len(temp_data):
-        continue
-      print(f'{speed_range_str} samples: {len(temp_data)}')
-      plt.figure()
-      angles, torque, speeds = zip(*[[line['angle_steers'], line['torque'], line['v_ego']] for line in temp_data])
-      plt.scatter(angles, torque, label=speed_range_str, color=color, s=0.05)
-
-      _x_ff = np.linspace(0, max(angles), res)
-      _y_ff = [feedforward(np.mean(speed_range), _i, old_kf) * MAX_TORQUE for _i in _x_ff]
-      plt.plot(_x_ff, _y_ff, color='orange', label='standard ff model at {} mph'.format(np.round(np.mean(speed_range) * CV.MS_TO_MPH, 1)))
-
-      _y_ff = [feedforward(np.mean(speed_range), _i, fit_kf) * MAX_TORQUE for _i in _x_ff]
-      plt.plot(_x_ff, _y_ff, color='purple', label='new fitted ff function')
-
-      plt.legend()
-      plt.xlabel('angle (deg)')
-      plt.ylabel('torque')
-      plt.savefig('plots/{}.png'.format(speed_range_str))
+  # data_speeds = np.array([line['v_ego'] for line in data])
+  # data_angles = np.array([line['angle_steers'] for line in data])
+  # data_torque = np.array([line['torque'] for line in data])
+  #
+  # params, covs = curve_fit(_fit_kf, np.array([data_speeds, data_angles]), np.array(data_torque) / MAX_TORQUE,
+  #                          # maxfev=800
+  #                          )
+  # fit_kf = params[0]
+  # print('FOUND KF: {}'.format(fit_kf))
+  #
+  # print()
+  #
+  # std_func = []
+  # fitted_func = []
+  # for line in data:
+  #   std_func.append(abs(feedforward(line['v_ego'], line['angle_steers'], old_kf) * MAX_TORQUE - line['torque']))
+  #   fitted_func.append(abs(feedforward(line['v_ego'], line['angle_steers'], fit_kf) * MAX_TORQUE - line['torque']))
+  # print('Function comparison on input data')
+  # print('Torque MAE, current vs. fitted: {}, {}'.format(round(np.mean(std_func), 3), round(np.mean(fitted_func), 3)))
+  # print('Torque STD, current vs. fitted: {}, {}'.format(round(np.std(std_func), 3), round(np.std(fitted_func), 3)))
+  #
+  # if SPEED_DATA_ANALYSIS := True:  # analyzes how torque needed changes based on speed
+  #   if PLOT_ANGLE_DIST := False:
+  #     sns.distplot([line['angle_steers'] for line in data if abs(line['angle_steers']) < 30], bins=200)
+  #     raise Exception
+  #
+  #   res = 100
+  #   color = 'blue'
+  #
+  #   _angles = [
+  #     [5, 10],
+  #     [10, 20],
+  #     [10, 15],
+  #     [15, 20],
+  #     [20, 25],
+  #     [20, 30],
+  #     [30, 45],
+  #   ]
+  #
+  #   for idx, angle_range in enumerate(_angles):
+  #     angle_range_str = '{} deg'.format('-'.join(map(str, angle_range)))
+  #     temp_data = [line for line in data if angle_range[0] <= abs(line['angle_steers']) <= angle_range[1]]
+  #     if not len(temp_data):
+  #       continue
+  #     print(f'{angle_range} samples: {len(temp_data)}')
+  #     plt.figure()
+  #     speeds, torque = zip(*[[line['v_ego'], line['torque']] for line in temp_data])
+  #     plt.scatter(np.array(speeds) * CV.MS_TO_MPH, torque, label=angle_range_str, color=color, s=0.05)
+  #
+  #     _x_ff = np.linspace(0, max(speeds), res)
+  #     _y_ff = [feedforward(_i, np.mean(angle_range), old_kf) * MAX_TORQUE for _i in _x_ff]
+  #     plt.plot(_x_ff * CV.MS_TO_MPH, _y_ff, color='orange', label='standard ff model at {} deg'.format(np.mean(angle_range)))
+  #
+  #     _y_ff = [feedforward(_i, np.mean(angle_range), fit_kf) * MAX_TORQUE for _i in _x_ff]
+  #     plt.plot(_x_ff * CV.MS_TO_MPH, _y_ff, color='purple', label='new fitted ff function')
+  #
+  #     plt.legend()
+  #     plt.xlabel('speed (mph)')
+  #     plt.ylabel('torque')
+  #     plt.savefig('plots/{}.png'.format(angle_range_str))
+  #
+  # if ANGLE_DATA_ANALYSIS := True:  # analyzes how angle changes need of torque (RESULT: seems to be relatively linear, can be tuned by k_f)
+  #   if PLOT_ANGLE_DIST := False:
+  #     sns.distplot([line['angle_steers'] for line in data if abs(line['angle_steers']) < 30], bins=200)
+  #     raise Exception
+  #
+  #   res = 100
+  #
+  #   _speeds = np.r_[[
+  #     [0, 10],
+  #     [10, 20],
+  #     [20, 30],
+  #     [30, 40],
+  #     [40, 50],
+  #     [50, 60],
+  #     [60, 70],
+  #   ]] * CV.MPH_TO_MS
+  #   color = 'blue'
+  #
+  #   for idx, speed_range in enumerate(_speeds):
+  #     speed_range_str = '{} mph'.format('-'.join([str(round(i * CV.MS_TO_MPH, 1)) for i in speed_range]))
+  #     temp_data = [line for line in data if speed_range[0] <= line['v_ego'] <= speed_range[1]]
+  #     if not len(temp_data):
+  #       continue
+  #     print(f'{speed_range_str} samples: {len(temp_data)}')
+  #     plt.figure()
+  #     angles, torque, speeds = zip(*[[line['angle_steers'], line['torque'], line['v_ego']] for line in temp_data])
+  #     plt.scatter(angles, torque, label=speed_range_str, color=color, s=0.05)
+  #
+  #     _x_ff = np.linspace(0, max(angles), res)
+  #     _y_ff = [feedforward(np.mean(speed_range), _i, old_kf) * MAX_TORQUE for _i in _x_ff]
+  #     plt.plot(_x_ff, _y_ff, color='orange', label='standard ff model at {} mph'.format(np.round(np.mean(speed_range) * CV.MS_TO_MPH, 1)))
+  #
+  #     _y_ff = [feedforward(np.mean(speed_range), _i, fit_kf) * MAX_TORQUE for _i in _x_ff]
+  #     plt.plot(_x_ff, _y_ff, color='purple', label='new fitted ff function')
+  #
+  #     plt.legend()
+  #     plt.xlabel('angle (deg)')
+  #     plt.ylabel('torque')
+  #     plt.savefig('plots/{}.png'.format(speed_range_str))
 
   # if PLOT_3D := False:
   #   X_test = np.linspace(0, max(data_speeds), 20)
@@ -287,4 +379,4 @@ if __name__ == '__main__':
   print(route_files)
   lr = MultiLogIterator(route_files)
 
-  n = main(lr)
+  model = main(lr)
