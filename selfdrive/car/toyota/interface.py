55fcad519d1a0dbd925ecb0701f79cd16e3cb509
sshane <shane@smiskol.com>
2022-03-25 04:18:34 -0700

    fix missing lat_params

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -62,7 +62,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 17.4
       tire_stiffness_factor = 0.5533
       ret.mass = 4387. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.LQR_RAV4)
+      set_lat_tune(ret.lateralTuning, lat_params, LatTunes.LQR_RAV4)
 
     elif candidate in (CAR.RAV4, CAR.RAV4H):
       stop_and_go = True if (candidate in CAR.RAV4H) else False
@@ -182,7 +182,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 16.0  # not optimized
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 3677. * CV.LB_TO_KG + STD_CARGO_KG  # mean between min and max
-      set_lat_tune(ret.lateralTuning, LatTunes.PID_D)
+      set_lat_tune(ret.lateralTuning, lat_params, LatTunes.PID_D)
 
     elif candidate == CAR.SIENNA:
       stop_and_go = True
