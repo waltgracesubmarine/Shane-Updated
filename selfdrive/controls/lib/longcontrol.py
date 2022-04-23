6560bdce5ed9da2f4443ddec3673f56b54b8ef02
Shane Smiskol <shane@smiskol.com>
2022-04-22 23:58:01 -0700

    one final fix?

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -56,8 +56,9 @@ class LongControl():
     self.long_control_state = LongCtrlState.off  # initialized to off
     self.pid = PIDController((CP.longitudinalTuning.kpBP, CP.longitudinalTuning.kpV),
                              (CP.longitudinalTuning.kiBP, CP.longitudinalTuning.kiV),
-                             (CP.longitudinalTuning.kdBP, CP.longitudinalTuning.kdV),
-                             k_f = CP.longitudinalTuning.kf, rate=1 / DT_CTRL,
+                             k_f=CP.longitudinalTuning.kf,
+                             k_d=(CP.longitudinalTuning.kdBP, CP.longitudinalTuning.kdV),
+                             rate=1 / DT_CTRL,
                              derivative_period=0.5)
     self.v_pid = 0.0
     self.last_output_accel = 0.0
