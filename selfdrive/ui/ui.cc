3bdba007bd42ab25b962edecb35e12eef280252c
Shane Smiskol <shane@smiskol.com>
2022-05-21 03:15:32 -0700

    try this

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -247,9 +247,9 @@ UIState::UIState(QObject *parent) : QObject(parent) {
 
   QTimer::singleShot(0, this, &UIState::update);
   // update timer
-//  QTimer *timer = new QTimer(this);
-//  QObject::connect(timer, &QTimer::timeout, this, &UIState::update);
-//  timer->start(1000 / UI_FREQ);
+  QTimer *timer = new QTimer(this);
+  QObject::connect(timer, &QTimer::timeout, this, &UIState::update);
+  timer->start(1000 / UI_FREQ);
 }
 
 void UIState::update() {
@@ -265,7 +265,7 @@ void UIState::update() {
   emit uiUpdate(*this);
   double remaining = next_frame_time - millis_since_boot();
   qDebug() << "Remaining:" << (remaining - draw_dt);
-  QTimer::singleShot(0, this, &UIState::update);
+//  QTimer::singleShot(0, this, &UIState::update);
 //  QTimer::singleShot(std::clamp(remaining, 0., 50.), this, &UIState::update);
 }
 
