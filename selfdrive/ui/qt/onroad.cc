62703cb8c88f00bdbea7d1c0e839161a3cd7191d
Shane Smiskol <shane@smiskol.com>
2022-05-23 22:56:43 -0700

    revert these changes
    fix

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -53,7 +53,6 @@ void OnroadWindow::updateState(const UIState &s) {
   }
 
   nvg->updateState(s);
-  nvg->update();
 
   if (bg != bgColor) {
     // repaint border
@@ -165,7 +164,7 @@ void OnroadAlerts::paintEvent(QPaintEvent *event) {
 
 // NvgWindow
 
-NvgWindow::NvgWindow(VisionStreamType type, QWidget* parent) : fps_filter(UI_FREQ, 3, 1. / UI_FREQ), CameraViewWidget("camerad", type, true, true, parent) {
+NvgWindow::NvgWindow(VisionStreamType type, QWidget* parent) : fps_filter(UI_FREQ, 3, 1. / UI_FREQ), CameraViewWidget("camerad", type, true, parent) {
   engage_img = loadPixmap("../assets/img_chffr_wheel.png", {img_size, img_size});
   dm_img = loadPixmap("../assets/img_driver_face.png", {img_size, img_size});
 }
@@ -403,7 +402,7 @@ void NvgWindow::paintGL() {
   double dt = cur_draw_t - prev_draw_t;
   double fps = fps_filter.update(1. / dt * 1000);
   if (fps < 15) {
-    LOGW("frame rate: %.2f fps", fps);
+    LOGW("slow frame rate: %.2f fps", fps);
   }
   prev_draw_t = cur_draw_t;
 }
