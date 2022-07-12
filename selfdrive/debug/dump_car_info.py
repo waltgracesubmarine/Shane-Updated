c7b128a84c7980ea428ed31eb1226c626f8e2aed
Shane Smiskol <shane@smiskol.com>
2022-07-11 20:51:19 -0700

    temp2
    add token
    
    test
    
    no token

--- selfdrive/debug/dump_car_info.py
+++ selfdrive/debug/dump_car_info.py
@@ -1,8 +1,13 @@
 import os
 import pickle
+from pathlib import Path
 
 from common.basedir import BASEDIR
 from selfdrive.car.docs import get_all_car_info
 
 with open(os.path.join(BASEDIR, '../old_car_info'), 'wb') as f:
   pickle.dump(get_all_car_info(), f)
+
+print('Dumping to {}'.format(os.path.join(BASEDIR, '../old_car_info')))
+
+Path('/tmp/test2').touch(exist_ok=True)
