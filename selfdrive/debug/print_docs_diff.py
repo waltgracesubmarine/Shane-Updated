18729e29ac4875b552e98534d5a07c503664d5b7
Shane Smiskol <shane@smiskol.com>
2022-07-11 22:35:01 -0700

    fix markdown

--- selfdrive/debug/print_docs_diff.py
+++ selfdrive/debug/print_docs_diff.py
@@ -6,7 +6,9 @@ from common.basedir import BASEDIR
 from selfdrive.car.docs import get_all_car_info
 from selfdrive.car.docs_definitions import Column
 
-STAR_ICON = "https://raw.githubusercontent.com/commaai/openpilot/master/docs/assets/icon-star-{}.svg"
+STAR_ICON = '<a href="##"><img valign="top" src="https://raw.githubusercontent.com/commaai/openpilot/master/docs/assets/icon-star-{}.svg" width="22" /></a>'
+COLUMNS = "|" + "|".join([column.value for column in Column]) + "|"
+COLUMN_HEADER = "|---|---|---|:---:|:---:|:---:|:---:|:---:|"
 
 
 def pretty_row(row, exclude=[Column.MAKE, Column.MODEL]):
@@ -24,7 +26,7 @@ def print_car_info_diff():
   base_car_info = {f'{i.make} {i.model}': i for i in base_car_info}
   new_car_info = {f'{i.make} {i.model}': i for i in get_all_car_info()}
 
-  markdown_builder = ["### ⚠️ This PR contains changes to [CARS.md](../blob/master/docs/CARS.md) ⚠️"]
+  markdown_builder = ["### ⚠️ This PR makes changes to [CARS.md](../blob/master/docs/CARS.md) ⚠️"]
 
   # TODO: car changing tiers
 
@@ -40,18 +42,20 @@ def print_car_info_diff():
   deleted_cars = set(base_car_info) - set(new_car_info)
   if len(deleted_cars):
     markdown_builder.append("# Removed")
-    markdown_builder.append("|".join([column.value for column in Column]))
+    markdown_builder.append(COLUMNS)
+    markdown_builder.append(COLUMN_HEADER)
     for k in deleted_cars:
       car_info = base_car_info[k]
-      markdown_builder.append("|".join([car_info.get_column(column, STAR_ICON, '{}') for column in Column]))
+      markdown_builder.append("|" + "|".join([car_info.get_column(column, STAR_ICON, '{}') for column in Column]) + "|")
 
   added_cars = set(new_car_info) - set(base_car_info)
   if len(added_cars):
     markdown_builder.append("# Added")
-    markdown_builder.append("|".join([column.value for column in Column]))
+    markdown_builder.append(COLUMNS)
+    markdown_builder.append(COLUMN_HEADER)
     for k in added_cars:
       car_info = new_car_info[k]
-      markdown_builder.append("|".join([car_info.get_column(column, STAR_ICON, '{}') for column in Column]))
+      markdown_builder.append("|" + "|".join([car_info.get_column(column, STAR_ICON, '{}') for column in Column]) + "|")
 
   print("\n".join(markdown_builder))
 
