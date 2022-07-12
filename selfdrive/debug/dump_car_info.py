3e7d7b284e3d153e77528e3afde1c5ef0e1347b5
Shane Smiskol <shane@smiskol.com>
2022-07-11 23:12:42 -0700

    Test changes

--- selfdrive/debug/dump_car_info.py
+++ selfdrive/debug/dump_car_info.py
@@ -5,6 +5,7 @@ import pickle
 from common.basedir import BASEDIR
 from selfdrive.car.docs import get_all_car_info
 
+# TODO: take path argument, or ideally make a jinja template somehow?
 with open(os.path.join(BASEDIR, '../openpilot_cache/old_car_info'), 'wb') as f:
   pickle.dump(get_all_car_info(), f)
 
--- selfdrive/debug/print_docs_diff.py
+++ selfdrive/debug/print_docs_diff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import pickle
+from collections import defaultdict
 
 from common.basedir import BASEDIR
 from selfdrive.car.docs import get_all_car_info
@@ -9,6 +10,15 @@ from selfdrive.car.docs_definitions import Column
 STAR_ICON = '<a href="##"><img valign="top" src="https://raw.githubusercontent.com/commaai/openpilot/master/docs/assets/icon-star-{}.svg" width="22" /></a>'
 COLUMNS = "|" + "|".join([column.value for column in Column]) + "|"
 COLUMN_HEADER = "|---|---|---|:---:|:---:|:---:|:---:|:---:|"
+ARROW_SYMBOL = "➡️"
+# EXCLUDE_COLUMNS = [Column.MAKE, Column.MODEL]  # these are used as keys, so exclude diffs
+
+# def combine_models(car_info):
+#   model_car_info = defaultdict(list)
+#   for car in car_info:
+#     print(car.hi)
+#     model_car_info[car.car_fingerprint].append(car)
+#   return model_car_info
 
 
 def pretty_row(row, exclude=[Column.MAKE, Column.MODEL]):
@@ -20,6 +30,18 @@ def load_base_car_info():
     return pickle.load(f)
 
 
+def get_diff(base_car, new_car):
+  # print(base_car.row)
+  # print(new_car.row)
+  diff = {}
+  # print(base_car.row == new_car.row)
+  for column, value in base_car.row.items():
+    if value != new_car.row[column]:
+      diff[column] = (value, new_car.row[column])
+  # print(diff)
+  return diff
+
+
 def print_car_info_diff():
   base_car_info = load_base_car_info()
 
@@ -28,8 +50,41 @@ def print_car_info_diff():
 
   markdown_builder = ["### ⚠️ This PR makes changes to [CARS.md](../blob/master/docs/CARS.md) ⚠️"]
 
-  # TODO: car changing tiers
+  # # TODO: car changing tiers
+  # # base_model_car_info = combine_models(load_base_car_info())
+  # new_model_car_info = combine_models(get_all_car_info())
+  # for car, cars in new_model_car_info.items():
+  #   print(len(cars))
+  #
+  # return
 
+  markdown_builder.append("# Changes")
+  for base_car_model, base_car in base_car_info.items():
+    if base_car_model not in new_car_info:
+      continue
+
+    new_car = new_car_info[base_car_model]
+    diff = get_diff(base_car, new_car)  # TODO: can just return new value (or old)
+    if not len(diff):
+      continue
+
+    row_builder = []
+    for column in Column:
+      if column not in diff:
+        row_builder.append(new_car.get_column(column, STAR_ICON, '{}'))
+      else:
+        row_builder.append(base_car.get_column(column, STAR_ICON, '{}') + ARROW_SYMBOL + new_car.get_column(column, STAR_ICON, '{}'))
+      # c = car_info.get_column(column, STAR_ICON, '{}') for column in Column
+
+    markdown_builder.append("|" + "|".join(row_builder) + "|")
+
+    # print(diff)
+    # print(row_builder)
+    # print()
+
+
+
+  # return
   # TODO: diffs in cars
   # TODO: once we identify changes, need to remove it from base_car_info and new_car_info so they don't pick up changes in make+model+years as new/removed cars
   # for new_car, new_car_info in new_car_info.items():
